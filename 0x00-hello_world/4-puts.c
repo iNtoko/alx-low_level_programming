d4f2aa2e2eb7106885791ff33e5cacaec09bbf94
iNtoko <innocentmohale@gmail.com>
2023-02-16 09:36:42 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,11 +8,10 @@
  */
 
 
- int main(void)
+int main(void)
 
 
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
