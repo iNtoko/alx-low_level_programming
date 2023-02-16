cfb6e0df47d392bde43250b9e52473eb18a19ce5
iNtoko <innocentmohale@gmail.com>
2023-02-16 10:05:15 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 
 
 /**
- * main - prints "with proper grammer, but the outcome is
+ * main - prints "with proper grammar, but the outcome is
  * a piece of art," followed by a newline
  * Return: Always 0.
  *
@@ -11,6 +11,6 @@
 int main(void)
 
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
