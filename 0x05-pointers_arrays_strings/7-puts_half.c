c90742433bd16d66fabd856e76e54ca4a1fb97f0
iNtoko <innocentmohale@gmail.com>
2023-02-27 14:09:04 -0800

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,38 +6,27 @@
  */
 
 void puts_half(char *str)
-
 {
-int len, n, i;
+	int j = 0;
+	int k;
 
-len = 0;
+	while (str[j] != '\0')
+	{
+	j++;
+	}
+	if (j % 2 == 1)
+	{
+	k = (j - 1) / 2;
+	k += 1;
+	}
+	else
+	{
+	k = j / 2;
+	}
 
-while
-	(str[len] != '\0');
-
-{
-len++;
-}
-if (len % 2 == 0)
-{
-for (i = len / 2; str[i] != '\0'; i++)
-{
-_putchar(str[i]);
-}
-else if (len % 2)
-
-{
-
-for
-	(n = (len - 1) / 2 n < len - 1; i++);
-{
-
-_putchar(str[n + 1]);
-
-}
-
-_putchar('\n');
-
-}
-
+	for (; k < j; k++)
+	{
+	_putchar(str[k]);
+	}
+	_putchar('\n');
 }
