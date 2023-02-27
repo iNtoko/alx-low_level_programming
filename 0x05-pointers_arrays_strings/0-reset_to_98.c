2c6398c763589a1deae48ee110389a28377b531c
iNtoko <innocentmohale@gmail.com>
2023-02-27 10:31:43 -0800

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - function that takes a pointer to an int as a parameter
  * and updates the value it points to 98
- * @n:An integer input 
+ * @n:An integer input
  *
  * Return:nothing
  */
