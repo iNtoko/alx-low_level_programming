04be58c2e780657ddf34d610f1567da020d93598
iNtoko <innocentmohale@gmail.com>
2023-03-06 03:18:45 -0800

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,11 +10,11 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i.
+	unsigned int i;
 
 	for (i = 0; n > 0; i++, n--)
 	{
-		dest[i] = src[i]
+		dest[i] = src[i];
 	}
 
 	return (dest);
